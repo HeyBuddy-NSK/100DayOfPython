40010fda7e1b5223257a25459b13f3e1303f1887
Nitin K <kendre.nitin2000@gmail.com>
2024-06-24 14:42:42 +0530

    Main file for app which defines shell processor for flask.

--- Day_50/nkblog/nkblog.py
+++ Day_50/nkblog/nkblog.py
@@ -1,10 +1,13 @@
 import os
 from app import create_app, db
-from app.models import User, Role
+from app.models import User
 from flask_migrate import Migrate
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app,db)
 
+with app.app_context():
+    db.create_all()
+
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User,Role=Role)
+    return dict(db=db, User=User)
