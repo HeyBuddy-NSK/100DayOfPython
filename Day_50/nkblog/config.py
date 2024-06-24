36e219906e1e77919f5b7bca228bd3edeb8d9276
Nitin K <kendre.nitin2000@gmail.com>
2024-06-24 14:41:47 +0530

    Configuration file for initializing environment variables and making one environment for developement, testing, production

--- Day_50/nkblog/config.py
+++ Day_50/nkblog/config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY','jsdfioisjthslkamcnfjrisl')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER','smtp.googleemail.com') 
+    MAIL_SERVER = os.environ.get('MAIL_SERVER','smtp.googlemail.com') 
     MAIL_PORT = int(os.environ.get('MAIL_PORT','587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS','true').lower() in ['true','on','1','True','TRUE']   
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
@@ -13,9 +13,10 @@ class Config:
     NKBLOG_MAIL_SENDER = 'Nkblog Admin nk@nkblog.com'
     NKBLOG_ADMIN = os.environ.get('NKBLOG_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # DEBUG = os.environ.get('DEBUG','1').lower() in ['true','on','1','True','TRUE']
 
     @staticmethod
-    def init_app():
+    def init_app(app):
         pass
 
 
